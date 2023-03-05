47cc4344d04fdeed2a4f7990657d94eca0c87c15
Nghia2603 <nghiand.ght@gmail.com>
2023-03-05 16:45:18 +0700

    update-bai-2

--- js/index.js
+++ js/index.js
@@ -17,3 +17,13 @@ document.getElementById('btn-tinh').onclick = function tinhDiemChuan(){
     function checkScore(e){
             return!(e <= 0);
     }
+
+// Bài 2
+document.getElementById('btn-tinhTienDien').onclick = function tinhTienDien(){
+    var name  = document.getElementById("nhap-ho-ten").value,
+    n = Number(document.getElementById("nhap-kw").value),
+    kw_1 = 500, kw_2 = 650, kw_3 = 850, kw_4 = 1100, kw_5 = 1300,
+    t = 0;
+    0 < n && n <= 50 ? t = n * kw_1 : n > 50 && n <= 100 ? t = 50 * kw_1 + (n - 50) * kw_2 : n > 100 && n <= 200 ? t = 50 * kw_1 + 50 * kw_2 + (n - 100) * kw_3 : n > 200 && n <= 350 ? t = 50 * kw_1 + 50 * kw_2 + 100 * kw_3 + (n-200) * kw_4 : n > 350 ? t = 50 * kw_1 + 50 * kw_2 + 100 * kw_3 + 150 * kw_4 + (n - 350) * kw_5 : alert("Số kw không hợp lệ! Vui lòng nhập lại");
+    t = new Intl.NumberFormat("vn-VN").format(t),document.getElementById("txt-ketQuaTinhTienDien").innerHTML = "Họ tên: "+ name +"; Tiền điện: " + t;
+}
